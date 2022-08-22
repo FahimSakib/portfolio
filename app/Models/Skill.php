c2e0c8fa4fac05ff3ef7afdfd948324da52c7d8d
FahimSakib <fsakib991@gmail.com>
2022-08-22 12:20:22 +0600

    Backend: skills initial

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'percentage', 'created_at', 'updated_at'];
 }
