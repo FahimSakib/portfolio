4773a535308e0bfca0e8327690b1edd06974c76e
FahimSakib <fsakib991@gmail.com>
2022-08-23 21:15:48 +0600

    changes

--- resources/views/frontend/sections/work.blade.php
+++ resources/views/frontend/sections/work.blade.php
@@ -11,7 +11,8 @@
                                     <div class="col-xl-7">
                                         <div class="experience-box pb-5">
                                             <div class="experience-content">
-                                                <div class="experience-number text-texture">{{ !empty($work->experience) ? $work->experience : '2' }}</div><br
+                                                <div class="experience-number text-texture">
+                                                    {{ !empty($work->experience) ? $work->experience : '2' }}</div><br
                                                     class="d-block d-sm-none">
                                                 <div class="experience-info">Years<br>Experience<br>Working
                                                 </div>
@@ -20,8 +21,9 @@
                                     </div>
                                     <div class="col-xl-5">
                                         <h2 class="title-uppercase"> <span class="text-primary">the
-                                                best</span> {{ !empty($work->heading) ? $work->heading : 'Website Around' }}</h2>
-                                                {{ !empty($work->heading) ? $work->heading : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' }}
+                                                best</span>
+                                            {{ !empty($work->heading) ? $work->heading : 'Website Around' }}</h2>
+                                        {{ !empty($work->heading) ? $work->heading : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' }}
                                         <div class="progress-bars">
                                             @foreach ($skills as $item)
                                             <div class="clearfix">
@@ -29,10 +31,11 @@
                                                 <div class="number float-right">{{ $item->percentage }}%</div>
                                             </div>
                                             <div class="progress">
-                                                <div class="progress-bar" role="progressbar" style="width: {{ $item->percentage }}%"
-                                                    aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div class="progress-bar" role="progressbar"
+                                                    style="width: {{ $item->percentage }}%" aria-valuenow="0"
+                                                    aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
-                                            @endforeach                                      
+                                            @endforeach
                                         </div>
                                     </div>
                                 </div>
