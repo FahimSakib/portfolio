2857bcacd9a978463b39b1f9d0e8f80739d53b98
FahimSakib <fsakib991@gmail.com>
2022-08-23 20:30:08 +0600

    data fatching to frontend initial

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Education;
+use App\Models\HeroSection;
+use App\Models\Project;
+use App\Models\Skill;
+use App\Models\Work;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -10,6 +16,13 @@ class IndexController extends Controller
     // $users = User::toBase()->get();
     public function index()
     {
-        return view('frontend.index');
+        $heroSection = HeroSection::toBase()->first();
+        $work        = Work::toBase()->first();
+        $skills      = Skill::toBase()->get();
+        $educations  = Education::toBase()->get();
+        $courses     = Course::toBase()->get();
+        $projects    = Project::toBase()->get();
+
+        return view('frontend.index',compact('heroSection','work','skills','educations','courses','projects'));
     }
 }
